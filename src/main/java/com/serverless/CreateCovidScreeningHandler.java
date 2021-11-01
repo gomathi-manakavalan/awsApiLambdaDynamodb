a5bfe96b463f5bc2cade75d74a701fdc619600b6
mail4bharane <mail4bharane@gmail.com>
2021-11-01 14:51:14 -0400

    update with new model for question and answer.

--- src/main/java/com/serverless/CreateCovidScreeningHandler.java
+++ src/main/java/com/serverless/CreateCovidScreeningHandler.java
@@ -5,58 +5,71 @@ import com.amazonaws.services.lambda.runtime.RequestHandler;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.serverless.dynamodb.CovidScreening;
+import com.serverless.dynamodb.QuestionAndAnswer;
 import org.apache.http.util.ExceptionUtils;
 import org.apache.log4j.Logger;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.util.Collections;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class CreateCovidScreeningHandler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
 
-	private final Logger logger = Logger.getLogger(this.getClass());
+    private final Logger logger = Logger.getLogger(this.getClass());
 
-	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+    @Override
+    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
 
-      try {
-          // get the 'body' from input
-          JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
+        try {
+            // get the 'body' from input
+            JsonNode body = new ObjectMapper().readTree((String) input.get("body"));
 
-          // create the CovidScreening object for post
-          CovidScreening covidScreening = new CovidScreening();
-          // covidScreening.setId(body.get("id").asText());
-          covidScreening.setName(body.get("name").asText());
-          covidScreening.setPhone(body.get("phone").asText());
-		  covidScreening.setDate(body.get("date").asText());
-		  covidScreening.setAnswer1(Boolean.valueOf(body.get("answer1").asText()));
-		  covidScreening.setAnswer2(Boolean.valueOf(body.get("answer2").asText()));
-			logger.info("quest and answer "+body.findValuesAsText("questionandAnswers"));
-          covidScreening.save(covidScreening);
+            // create the CovidScreening object for post
+            CovidScreening covidScreening = new CovidScreening();
+            // covidScreening.setId(body.get("id").asText());
+            covidScreening.setName(body.get("name").asText());
+            covidScreening.setPhone(body.get("phone").asText());
+            covidScreening.setDate(getCurrentDate());
 
-          // send the response back
-      		return ApiGatewayResponse.builder()
-      				.setStatusCode(200)
-      				.setObjectBody(covidScreening)
-      				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
-      				.build();
+            logger.debug("quest and answer " + body.findValues("questionandAnswers"));
+            ObjectMapper mapper = new ObjectMapper();
+            String qandaString = mapper.writeValueAsString(body.get("questionandAnswers"));
+            logger.debug("Q and A string value"+qandaString);
+            List<QuestionAndAnswer> questionAndAnswerList = mapper.readValue(qandaString, new TypeReference<List<QuestionAndAnswer>>() {});
+            covidScreening.setQuestionAndAnswers(questionAndAnswerList);
+            covidScreening.save(covidScreening);
 
-      } catch (Exception ex) {
-		  StringWriter stringWriter= new StringWriter();
-		  PrintWriter printWriter= new PrintWriter(stringWriter);
-		  ex.printStackTrace(printWriter);
-		  String stackTraceAsString= stringWriter.toString();
-		  logger.error("Error in saving covidScreening3: " + stackTraceAsString);
+            // send the response back
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(200)
+                    .setObjectBody(covidScreening)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
 
-          // send the error response back
-    			Response responseBody = new Response("Error in saving covidScreening3: ", input);
-    			return ApiGatewayResponse.builder()
-    					.setStatusCode(500)
-    					.setObjectBody(responseBody)
-    					.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
-    					.build();
-      }
-	}
+        } catch (Exception ex) {
+            StringWriter stringWriter = new StringWriter();
+            PrintWriter printWriter = new PrintWriter(stringWriter);
+            ex.printStackTrace(printWriter);
+            String stackTraceAsString = stringWriter.toString();
+            logger.error("Error in saving covidScreening3: " + stackTraceAsString);
+
+            // send the error response back
+            Response responseBody = new Response("Error in saving covidScreening3: ", input);
+            return ApiGatewayResponse.builder()
+                    .setStatusCode(500)
+                    .setObjectBody(responseBody)
+                    .setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & Serverless"))
+                    .build();
+        }
+    }
+
+    private String getCurrentDate() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
 }
